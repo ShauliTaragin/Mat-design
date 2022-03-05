e883fc6c3e20d01bd30403b5f5d80dfefcb02a0a
ShauliTaragin <shaulitaragin@gmail.com>
2022-03-05 19:54:05 +0200

    code should be working now want to intergrate with demo.cpp

--- mat.cpp
+++ mat.cpp
@@ -3,35 +3,61 @@
 
 using namespace std;
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
-}
+//int main() {
+//    cout << ariel::mat(15, 31, '@', '-') << endl;
+//    return 0;
+//}
 
 namespace ariel {
-
     string mat(int columns, int rows, char symb1, char symb2) {
-        char **matrix = new char *[rows];
-        for (int i = 0; i < rows; ++i) {
-            matrix[i] = new char[columns];
-        }
-        bool flag= true;
-        string to_print = HelperMat(columns, rows, symb1, symb2, columns, matrix,true);
-    }
-
-    string HelperMat(int c, int r, char symb1, char symb2, int size, char ** matrix, bool flag) {
-        if(c==1||r==1){//base case
-            if(c==1){
-                for(int i=0 ; i<r ; i++){
-
+        if (columns % 2 == 0 || rows % 2 == 0||rows < 1 || columns < 1) {
+            throw "Mat size is always odd ";
+        } else {
+            char **matrix = new char *[rows];
+            for (int i = 0; i < rows; ++i) {
+                matrix[i] = new char[columns];
+            }
+            bool flag = true;
+            string result = "";
+            //I will implement this function by working like an onion i.e. I will create each frame separately
+            //starting from external frame until the final center frame.
+            int start_rows = 0;
+            int start_columns = 0;
+            int end_rows = rows;
+            int end_columns = columns;
+            int row_counter = rows;
+            int col_counter = columns;
+            while (col_counter > 0 && row_counter > 0) {
+                for (int i = start_rows; i < end_rows; ++i) {
+                    for (int j = start_columns; j < end_columns; ++j) {
+                        if (i == start_rows || i == end_rows - 1 || j == start_columns || j == end_columns - 1) {
+                            if (flag)
+                                matrix[i][j] = symb1;
+                            else
+                                matrix[i][j] = symb2;
+                        }
+                    }
+                }
+                if (flag)//want to make sure to switch between symbols
+                    flag = false;
+                else
+                    flag = true;
+                start_columns++;
+                start_rows++;
+                end_columns--;
+                end_rows--;
+                row_counter -= 2;
+                col_counter -= 2;
+            }
+            //now all thats left is to make the matrix into a string which is printable easily
+            for (int i = 0; i < rows; ++i) {
+                if (i != 0)
+                    result += '\n';
+                for (int j = 0; j < columns; ++j) {
+                    result += matrix[i][j];
                 }
             }
-            else{
-                for(int i=0 ; i<c ; i++){
-
-                }
-            }
+            return result;
         }
-        else
     }
 }
--- mat.h
+++ mat.h
@@ -6,8 +6,8 @@
 #define EX1_MAT_H
 
 using namespace std;
+
 namespace ariel{
     string mat(int columns, int rows , char symb1 , char symb2);
-    string HelperMat(int c, int r, char symb1, char symb2 ,int size, char ** matrix,bool flag);
 }
 #endif //EX1_MAT_H
