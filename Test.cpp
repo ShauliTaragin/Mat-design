2a8c6c1754957d49a56c7534cf30de4c534eb56e
ShauliTaragin <shaulitaragin@gmail.com>
2022-03-06 11:34:41 +0200

    changes to test

---
+++ Test.cpp
@@ -0,0 +1,30 @@
+#include "doctest.h"
+#include "mat.hpp"
+
+using namespace ariel;
+
+#include <string>
+#include <stdexcept>
+#include <algorithm>
+
+using namespace std;
+
+string nospaces(string input) {
+    input.erase(remove(input.begin(), input.end(), ' '), input.end());
+    input.erase(remove(input.begin(), input.end(), '\t'), input.end());
+    input.erase(remove(input.begin(), input.end(), '\n'), input.end());
+    input.erase(remove(input.begin(), input.end(), '\r'), input.end());
+    return input;
+}
+
+TEST_CASE("Good input") {
+            CHECK(nospaces(mat(9, 7, '@', '-')) == nospaces("@@@@@@@@@\n"
+                                                            "@-------@\n"
+                                                            "@-@@@@@-@\n"
+                                                            "@-@---@-@\n"
+                                                            "@-@@@@@-@\n"
+                                                            "@-------@\n"
+                                                            "@@@@@@@@@"));
+    /* Add more test here */
+}
+
